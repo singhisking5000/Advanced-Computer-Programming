3b17d78c256f3520e6fa94826f64d6770c0c33af
singhisking5000 <jamshedkalsi51@gmail.com>
2025-08-29 22:28:22 -0400

    im gonna commit arson

--- CeaserCypher.java
+++ CeaserCypher.java
@@ -1,22 +1,27 @@
 
 public class CeaserCypher {
-    
-    private String[] alphabet = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
-    
-    public String ceaserCypher(String encoded, int distance)
+    public static String ceaserCypher(String encoded, int distance)
     {
-        encoded.toUpperCase();
+        encoded = encoded.toUpperCase();
 
+        char[] alphabet = {'A','B','C','D','E','F','G','H','I','J','K','J','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
         String decodedString = "";
 
-        for (int i = 0; i < alphabet.length; i++)
-        {
-            if ((i + distance) < 26)
+        for (int encodedLetter = 0; encodedLetter < encoded.length(); encodedLetter++)
+        {   
+            for (int letterIndex = 0; letterIndex < alphabet.length; letterIndex++)
             {
-                decodedString += alphabet[(i + distance)];
-            } else
-            {
-                decodedString += alphabet[(i + distance) - 26];
+                if (encoded.charAt(encodedLetter) == alphabet[letterIndex])
+                {
+                    if ((letterIndex + distance) < 26)
+                    {
+                        decodedString += alphabet[letterIndex + distance];
+                    } else
+                    {
+                        decodedString += alphabet[(letterIndex + distance) - 26];
+                    }
+                    
+                }
             }
         }
 
---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) 
+    {    
+        System.out.println("--------MAIN--------\n\n\n\n");
+        System.out.println(CeaserCypher.ceaserCypher("map", 2) + " <--- Cyphered code");
+        System.out.println("\n\n\n\n--------MAIN--------");  
+    }
+}
