39b3754e0a63b3aaca239386903861c1b23bfba3
singhisking5000 <jamshedkalsi51@gmail.com>
2025-09-02 23:27:40 -0400

    Absolutely suffered for more than 3 hours :D

--- CeaserCypher.java
+++ CeaserCypher.java
@@ -1,5 +1,21 @@
 
 public class CeaserCypher {
+
+    /*  
+     *  A class for the a Ceaser Cypher Object
+     *  (I am aware that this is spelled super wrong, but I
+     *  realized this after I already coded all of this) 
+     * 
+     *  The Ceaser Cypher Object has a static function that 
+     *  performs a ceaser cypher... suprising right?
+     * 
+     *  Put in encoded (or data to be encoded) and put in
+     *  a digit to shift the letters in the alphabet.
+     * 
+     *  Written by Jamshed Kalsi 9/2/2025
+     *  Advanced Computer Programming III
+     */
+
     public static String ceaserCypher(String encoded, int distance)
     {
         encoded = encoded.toUpperCase();
