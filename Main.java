5a380ad73c00ecc268d0e5238f6351cfb8bbb26b
singhisking5000 <73306374+singhisking5000@users.noreply.github.com>
2025-09-04 16:42:20 +0000

    Level 3 complete

--- Main.java
+++ Main.java
@@ -2,9 +2,11 @@ public class Main {
 
     //public static MapOfSufferingAndDoom useMe;
     public static MapOfSufferingAndDoom thingy;
+    public static LevelThree anotherone;
     public static void main(String[] args) 
     {    
         thingy.getCounts();
+        anotherone.patternFinder();
         System.out.println("--------MAIN--------\n\n\n\n");
         System.out.println(CeaserCypher.ceaserCypher("map", 2) + " <--- Cyphered code");
         System.out.println("\n\n\n\n--------MAIN--------");  
